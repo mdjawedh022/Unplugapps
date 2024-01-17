c8fc55a75834eb21a5c48af80720350e3bb4eb7e
Md Jawed <mdjawedh22@gmail.com>
2024-01-17 16:46:13 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Routes } from 'react-router-dom';
+import './App.css';
+import Table from './Table';
+
+function App() {
+  return (
+    <div className="App">
+     {/* <h1>Practical Assessment</h1> */}
+     <Table/>
+ <Routes>
+{/* <Route path="/" element={< />}></Route> */}
+ </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Table.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header'
+import Details from './components/Details'
+
+const Table = () => {
+  return (
+    <div>
+      <Header/>
+      <Details/>
+    </div>
+  )
+}
+
+export default Table
